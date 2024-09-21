97a539253c0190685a7b5bb37b5ef310e3ce80c6
Charles Hug <charleshug@gmail.com>
2024-09-21 14:46:51 -0700

    updated reports

--- app/services/spending_by_vendor_report_service.rb
+++ app/services/spending_by_vendor_report_service.rb
@@ -1,5 +1,6 @@
 class SpendingByVendorReportService
   def self.get_hash_trx_by_vendor_cat(trxes_input)
+    return {} if trxes_input.empty?
     trx_amounts = SpendingByVendorReportService.get_hash_trx_by_vendor(trxes_input)
     trx_amounts.sort_by { |_, amount| amount }.to_h
     # Use the following line for descending order:
@@ -7,12 +8,23 @@ class SpendingByVendorReportService
   end
 
   def self.get_hash_trx_by_vendor(trxes_input)
-    trx_amounts =     trxes_input.joins(:vendor, subcategory: :category)
-    .where.not(vendor: { account_id: nil })  # Exclude trx where vendor's account is nil
-    .where.not(categories: { normal_balance: "INCOME" })  # Exclude trx where category is INCOME
-    .group("vendor.id", "vendor.name")  # Group by vendor_id and vendor name
-    .order("SUM(amount) DESC")  # Order by the total sum of amounts for each vendor
-    .sum(:amount)  # Sum the amounts for each vendor
+    # return {} if trxes_input.empty?
+    # trx_amounts =     trxes_input
+    # .joins(lines: { ledger: { subcategory: :category }, trx: :vendor })
+    # .where(lines: { line_transfer_id: nil })  # Exclude trx whose lines have a value in line_transfer_id
+    # # .where.not(vendor: { account_id: nil })  # Exclude trx where vendor's account is nil
+    # # .where.not(categories: { normal_balance: "INCOME" })  # Exclude trx where category is INCOME
+    # .group("vendors.id", "vendors.name")  # Group by vendor_id and vendor name
+    # # .order("SUM(lines.amount) DESC")  # Order by the total sum of amounts for each vendor
+    # .sum("trxes.amount")  # Sum the amounts for each vendor
+    # trx_amounts
+
+    return {} if trxes_input.empty?
+    trx_amounts = trxes_input
+      .joins(lines: { ledger: { subcategory: :category }, trx: :vendor })
+      .where(lines: { transfer_line_id: nil })  # Exclude trx whose lines have a value in line_transfer_id
+      .group("vendors.id", "vendors.name")  # Group by vendors.id and vendors.name
+      .sum("trxes.amount")  # Sum the amounts for each vendor
     trx_amounts
   end
 
