878734326cce9cb3039b9f9b2f3ab6f74930b629
Charles Hug <charleshug@gmail.com>
2025-03-12 22:43:15 -0700

    change latest migration file causing trouble adding order again

--- db/migrate/20250311051942_add_order_to_categories.rb
+++ db/migrate/20250311051942_add_order_to_categories.rb
@@ -1,26 +1,28 @@
 class AddOrderToCategories < ActiveRecord::Migration[7.2]
-  def up
-    add_column :categories, :order, :integer
+  def change
+    unless column_exists?(:categories, :order)
+      add_column :categories, :order, :integer
 
-    # Set initial order values for existing categories
-    execute <<-SQL
-      UPDATE categories c
-      SET "order" = subquery.row_number
-      FROM (
-        SELECT id, budget_id, ROW_NUMBER() OVER (PARTITION BY budget_id ORDER BY created_at) as row_number
-        FROM categories
-      ) AS subquery
-      WHERE c.id = subquery.id
-    SQL
+      # Set initial order values for existing categories
+      execute <<-SQL
+        UPDATE categories c
+        SET "order" = subquery.row_number
+        FROM (
+          SELECT id, budget_id, ROW_NUMBER() OVER (PARTITION BY budget_id ORDER BY created_at) as row_number
+          FROM categories
+        ) AS subquery
+        WHERE c.id = subquery.id
+      SQL
 
-    # Make the column non-nullable after setting values
-    change_column_null :categories, :order, false
+      # Make the column non-nullable after setting values
+      change_column_null :categories, :order, false
 
-    add_index :categories, [ :budget_id, :order ], unique: true
+      add_index :categories, [ :budget_id, :order ], unique: true
+    end
   end
 
-  def down
-    remove_index :categories, [ :budget_id, :order ]
-    remove_column :categories, :order
-  end
+  # def down
+  #   remove_index :categories, [ :budget_id, :order ]
+  #   remove_column :categories, :order
+  # end
 end
