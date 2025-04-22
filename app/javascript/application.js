630b88edf7d6e5635b5f4fb6c033116afff6b42a
Charles Hug <charleshug@gmail.com>
2025-04-21 17:03:32 -0700

    updated javascript importmaps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "chartkick"
 import "controllers"
+
+import Chart from "chart.js/auto"
+// Assign to global so Chartkick can see it
+window.Chart = Chart
